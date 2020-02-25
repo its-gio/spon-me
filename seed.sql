b8c01c407f86c40f38119fa582f2bbed05a8b0f3
Giovani Aguirre <me@its-g.io>
2020-02-25 11:42:55 -0600

    Updated Seed.sql for easier reset

--- seed.sql
+++ seed.sql
@@ -1,24 +1,35 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS events;
+
+CREATE TABLE events
+(
+  event_id SERIAL PRIMARY KEY,
+  category VARCHAR(50),
+  location TEXT
+);
 
 CREATE TABLE users
 (
   user_id SERIAL PRIMARY KEY,
-  event_id INT REFERENCES event
+  event_id INT REFERENCES events
 (event_id),
-  f_name VARCHAR
+  first_name VARCHAR
 (50),
-  l_name VARCHAR
+  last_name VARCHAR
 (40),
-  email TEXT,
+  email TEXT UNIQUE,
+  hash TEXT,
   discussion_leader BOOLEAN,
   available BOOLEAN,
   hasArrived BOOLEAN
 );
 
-CREATE TABLE event
-(
-  event_id SERIAL PRIMARY KEY,
-  category VARCHAR(50),
-  location TEXT,
-  -- duration TIME
-);
+-- INSERT INTO users
+--   (first_name, last_name, email, hash)
+-- VALUES
+--   (
+--     "Gio",
+--     "Aguirre",
+--     "me@its-g.io",
+--     "adsouaf9383hr97htgreuhiurp9ufp981hp9fhepvuid923hr9pefeuwbywbef"
+--   )
