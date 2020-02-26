703fb929ca6bb7bd5ae97b85bd733b127720e2fd
Giovani Aguirre <me@its-g.io>
2020-02-26 15:31:12 -0600

    Created react hook and connected .getCurrentPosition as default when component mounted.

--- src/components/UserMap.jsx
+++ src/components/UserMap.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl'
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl'
 
 export default function UserMap() {
   const [viewport, setViewport] = useState({
-    latitude: 32.7759944,
-    longitude: -96.7962258,
+    latitude: 0,
+    longitude: 0,
     width: '100vw',
     height: '100vh',
     zoom: 17
   })
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) =>  {
+        const {latitude, longitude} = position.coords;
+        setViewport({...viewport, latitude, longitude})
+      })
+    } else {
+      console.error('Geolocation is not supported!')
+    }
+  }, [])
 
   return (
     <div>
@@ -16,9 +27,12 @@ export default function UserMap() {
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
         mapStyle='mapbox://styles/its-gio/ck73qpski1ssc1io7mtpotq0i'
-        onViewportChange={viewport => setViewport(viewport)}
+        onViewportChange={viewport => {
+            setViewport(viewport)
+          }
+        }
+        
       >
-        Markers!
       </ReactMapGL>
     </div>
   )
