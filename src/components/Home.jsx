d8f84d2cfa7920a29cae6bc31eaf965c519642de
Giovani Aguirre <me@its-g.io>
2020-02-27 10:45:09 -0600

    Set up for register section complete. Styling is next.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { FaMapMarker, FaStar , FaUserPlus } from 'react-icons/fa';
 
 export default class Home extends Component {
   render() {
@@ -18,6 +19,39 @@ export default class Home extends Component {
             <h5>Connect with people for no reason at all!</h5>
           </div>
         </header>
+
+        <div className="register">
+          <h2>Register Meow!</h2>
+          <div className="register-content">
+            <div className="register-content--features">
+              <div className="register-content--features__item">
+                <FaMapMarker />
+                <h6>Drop a beacon and meet your neighbours</h6>
+              </div>
+
+              <div className="register-content--features__item">
+                <FaUserPlus />
+                <h6>Make personal connections</h6>
+              </div>
+
+              <div className="register-content--features__item">
+                <FaStar />
+                <h6>Be spontaneous!</h6>
+              </div>
+            </div>
+
+            <div className="register-content--form">
+              <input placeholder="First Name" type="text"/>
+              <input placeholder="Last Name" type="text"/>
+              <input placeholder="Email" type="email"/>
+              <input placeholder="Password" type="password"/>
+              <input placeholder="MM" type="text"/>
+              <input placeholder="DD" type="text"/>
+              <input placeholder="YYYY" type="text"/>
+              <button>Sign Up</button>
+            </div>
+          </div>
+        </div>
       </section>
     )
   }
