6850c0d09783b12bee05c3e0227a3a7450b95ced
Giovani Aguirre <me@its-g.io>
2020-03-02 11:27:37 -0600

    Added functions for change and submit. Next Step: Adding endpoint.

--- src/components/Modal/UserEditModal.jsx
+++ src/components/Modal/UserEditModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 const modalRoot = document.getElementById('modal-root');
 
@@ -24,10 +25,18 @@ class Modal extends React.Component {
   componentWillUnmount() {
     modalRoot.removeChild(this.el);
   }
+  
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(this.state)
+  }
 
   render () {
     return ReactDOM.createPortal(
-      <form className="form">
+      <form onSubmit={this.handleSubmit} className="form">
         <input required onChange={this.handleChange} className="form__name" value={this.state.first_name} name="first_name" placeholder="First Name" type="text"/>
         <input required onChange={this.handleChange} className="form__name" value={this.state.last_name} name="last_name" placeholder="Last Name" type="text"/>
         <input required onChange={this.handleChange} className="form__email" value={this.state.email} name="email" placeholder="Email" type="email"/>
