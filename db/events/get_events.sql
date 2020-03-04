37130c47626006bacd752edef273145993fbb095
Giovani Aguirre <me@its-g.io>
2020-03-04 16:23:59 -0600

    Updated get sql query for events on seed and db folder.

--- db/events/get_events.sql
+++ db/events/get_events.sql
@@ -1,3 +1,3 @@
-SELECT *
+SELECT discussion_leader, category, description, long, lati, TO_CHAR(start_time :: TIME, 'HH:MI am') as start_time, TO_CHAR(end_time :: TIME, 'HH:MI am') as end_time
 FROM events
 WHERE end_time > timezone('CST', current_timestamp);
--- seed.sql
+++ seed.sql
@@ -52,4 +52,6 @@ CREATE TABLE events
 -- 	"end_time": "2020-3-4 12:00:00"
 -- }
 
--- select TO_CHAR(start_time :: TIME, 'HH:MI am') as start_time, TO_CHAR(end_time :: TIME, 'HH:MI am') as end_time
+-- SELECT discussion_leader, category, description, long, lati, TO_CHAR(start_time :: TIME, 'HH:MI am') as start_time, TO_CHAR(end_time :: TIME, 'HH:MI am') as end_time
+-- FROM events
+-- WHERE end_time > timezone('CST', current_timestamp);
