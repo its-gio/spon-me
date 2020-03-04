171ac6ce88e1419ae38ec569460bb5f63b786512
Giovani Aguirre <me@its-g.io>
2020-03-04 17:02:39 -0600

    Switched sendStatus with status

--- server/controllers/eventsController.js
+++ server/controllers/eventsController.js
@@ -2,12 +2,16 @@ async function createEvent(req, res) {
   const { category, description, long, lati, start_time, end_time } = req.body;
   const db = req.app.get('db');
 
-  const createdEvent = db.events.create_event(req.session.user.user_id, category, description, long, lati, start_time, end_time).catch(() => res.sendStatus(501).json('Error when creating Event'));
-  res.sendStatus(200).json(createdEvent[0]);
+  const createdEvent = db.events.create_event(req.session.user.user_id, category, description, long, lati, start_time, end_time).catch(() => res.status(501).json('Error when creating Event'));
+  res.status(200).json(createdEvent[0]);
 }
 
-async function getEvents(res, req) {
+async function getEvents(req, res) {
+  const db = req.app.get('db');
 
+  const gottenEvents = await db.events.get_events().catch(() => res.status(501).json('Get Events failed'))
+
+  res.status(200).json(gottenEvents);
 }
 
 module.exports = {
