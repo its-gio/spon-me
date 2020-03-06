25e4e5d91182464b822a9b2f863933d2c9c24a27
Giovani Aguirre <me@its-g.io>
2020-03-05 22:41:23 -0600

    AttendeesList now shows when event starts and hides when event has not started.

--- src/components/UserMap/Map/EventPopups.jsx
+++ src/components/UserMap/Map/EventPopups.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Popup } from 'react-map-gl';
+import AttendeesList from './AttendeesList';
+
 
 function EventPopups(props) {
+  const checkTime = () => {
+    const today = new Date();
+    const clockTime = `${today.getHours()}${today.getMinutes()}`;
+    const timeSplit = props.selectedEvent.start_time.split(" ");
+    const startTime = timeSplit[1] === 'am' ? timeSplit[0].split(":").join('') : +timeSplit[0].split(':').join('') + 1200;
+    return clockTime > startTime;
+  }
+
   return (
     <>
       { props.selectedEvent ? (
         <Popup offsetTop={-55} latitude={props.selectedEvent.lati} longitude={props.selectedEvent.long} onClose={() => props.setSelectedEvent(null)}>
-          <h6>Category: {props.selectedEvent.category}</h6>
+          <h5>Category: {props.selectedEvent.category}</h5>
           {props.selectedEvent.description ? <p>{props.selectedEvent.description}</p> : ""}
-          <p>Start:{props.selectedEvent.start_time}</p>
-          <p>End:{props.selectedEvent.end_time}</p>
-          <p>Current Group:</p>
-          {/* <ul>
-            <li></li>
-          </ul> */}
+          <p>Start: {props.selectedEvent.start_time}</p>
+          <p>End: {props.selectedEvent.end_time}</p>
+          {
+            checkTime() ?
+            <AttendeesList attendees={props.selectedEvent.attendees} /> :
+            null
+          }
         </Popup>
       ) : null } 
     </>
